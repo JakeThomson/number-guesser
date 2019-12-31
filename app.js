9fa8acb2226e94efe95d7189058801851188d144
Jake Thomson <jakethomson0@gmail.com>
2019-12-31 11:34:21 +0000

    Added warmth hints

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // Game values
 let min = 1,
-    max = 10,
+    max = 1000,
+    guess,
+    lastGuess,
+    warmth = 0;
     winningNum = getRandomNum(min, max),
-    guessesLeft = 3;
+    guessesLeft = 12;
 
+    console.log(winningNum);
 // UI elements
 const game = document.querySelector("#game"),
       minNum = document.querySelector(".min-num"),
@@ -25,7 +29,7 @@ game.addEventListener("mousedown", function(e){
 
 // Listen for guess
 guessBtn.addEventListener("click", function(){
-  let guess = parseInt(guessInput.value);
+  guess = parseInt(guessInput.value);
 
   // Validate
   if(isNaN(guess) || guess < min || guess > max){
@@ -40,16 +44,19 @@ guessBtn.addEventListener("click", function(){
     guessesLeft -= 1;
 
     if(guessesLeft === 0){
-      gameOver(false, `Game over, You lost. The correct answer was ${winningNum}`);
+      gameOver(false, `Game over. The correct answer was ${winningNum}`);
     } else {
       // Wrong answer - game continues
 
       //Tell user guess was incorrect
       guessInput.style.borderColor = "red";
-      setMessage(`${guessInput.value} is not correct, you have ${guessesLeft} guesses left.`, "red");
+      if(lastGuess === undefined)
+      setMessage(`${guess} is incorrect, you have ${guessesLeft} guesses left.`, "red");
+      else
+      setMessage(`${getWarmth()}. you have ${guessesLeft} guesses left.`, "red");
 
       // Clear input
-      guessInput.value = "";
+      lastGuess = guess;
       }
     }
   }
@@ -69,6 +76,80 @@ function gameOver(won, msg){
   // Play again?
   guessBtn.value = "Play Again";
   guessBtn.className += "play-again";
+}
+
+// Determine warmth
+function getWarmth(){
+  const guessDifference = Math.abs(winningNum-guess);
+  const lastGuessDifference = Math.abs(winningNum-lastGuess);
+  let word = "",
+      warmthWord,
+      returnMsg;
+
+  if(guessDifference>lastGuessDifference){
+    if(warmth>0)
+      warmth = 0;
+    warmth -= 1;
+  } else {
+    if(warmth<0)
+      warmth = 0;
+    warmth += 1;
+  }
+
+  switch(warmth){
+    case 1:
+      warmthWord = "warmer.";
+      break;
+    case 2:
+      warmthWord = "warmer!";
+      break;
+    case 3:
+      warmthWord = "even warmer!";
+      break;
+    case 4:
+      warmthWord = "warmeerrrrr!!!";
+      warmth = 1;
+      break;
+    case -1:
+      warmthWord = "colder.";
+      break;
+    case -2:
+      warmthWord = "colder!";
+      break;
+    case -3:
+      warmthWord = "even colder, what are you doing?!";
+      break;
+    case -4:
+      warmthWord = "SO COLD. GO THE OTHER WAY.";
+      warmth = -2;
+      break;
+  }
+
+  if(guessDifference <= Math.ceil((max-min)*0.005)){
+    word = "HOTTER THAN THE SUN!!!!!";
+  } else if(guessDifference <= Math.ceil((max-min)*0.02)){
+    word = "SHIT HOT!!";
+  } else if(guessDifference <= Math.ceil((max-min)*0.05)){
+    word = "super warm!";
+  } else if(guessDifference <= Math.ceil((max-min)*0.15)){
+    word = "pretty warm!";
+  } else if(guessDifference <= Math.ceil((max-min)*0.25)){
+    word = "warm!";
+  } else if(guessDifference >= Math.ceil((max-min)*0.9)){
+    word = "getting frostbite.";
+  } else if(guessDifference >= Math.ceil((max-min)*0.8)){
+    word = "very very cold.";
+  } else if(guessDifference >= Math.ceil((max-min)*0.7)){
+    word = "quite cold";
+  } else if(guessDifference >= Math.ceil((max-min)*0.5)){
+    word = "cold";
+  } else {
+    word = "lukewarm";
+  }
+
+  returnMsg = `Getting ${warmthWord} You're ${word}`;
+
+  return returnMsg;
 }
 
 // Get Winning Number
